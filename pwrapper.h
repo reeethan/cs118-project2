5c1b129c694d882bd21ef366a8e4d6ff792cb2b9
adrayv <draym001@g.ucla.edu>
2018-03-15 23:18:15 -0700

    server has queue for the window. wrapper class for packet

---
+++ pwrapper.h
@@ -0,0 +1,21 @@
+#ifndef PWRAPPER_H
+#define PWRAPPER_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "packet.h"
+
+#define TIMESTAMP unsigned long long
+
+struct pwrapper {
+  char completed;
+  TIMESTAMP ts;
+  struct packet packet;
+};
+
+TIMESTAMP getTime(); // GETS CURRENT TIME
+void printWrapper(struct pwrapper *);
+struct pwrapper* createPwrapper(struct packet*);
+
+#endif
